287cf72f802277ceaf829917e7e9358d383cb581
Vincent Mathew <vincent.mathew@myntra.com>
2016-08-02 10:46:53 +0530

    Almost final state

--- components/todolist_item.js
+++ components/todolist_item.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import { AppRegistry, ListView, Text, View } from 'react-native';
 import Button from 'react-native-button';
+import CheckBox from 'react-native-checkbox';
 
 export default class TODOListItem extends Component {
 
     constructor(props){
       super(props);
 
+      this.state = { isChecked : false};
     }
+
+    onCheckChange(){
+      console.log("checkbox onClick called onCheckChange");
+      console.log("onChange is called");
+      this.setState({isChecked : !this.state.isChecked});
+    }
+
 
 
     render(){
       // making a view with text and Button
-      // TODO: add a checkbox 
+      // TODO: add a checkbox
       return(
         <View style={{flex:1, flexDirection:'row', alignItems: 'center', justifyContent: 'center' }}>
 
+          <CheckBox label='Remeber Me' checked = {this.state.isChecked} onChange={this.onCheckChange.bind(this)} />
           <Text style={{flex:8, alignItems: 'center',justifyContent: 'center'}}>{this.props.textData}</Text>
           <Button
             style={{borderWidth: 1, borderColor: 'blue', flex : 2, alignItems: 'center'}}
